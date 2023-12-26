ce3c80197fbb48474b73cb8c815d4337e7cc98a3
Beloff Lab <belofflab@gmail.com>
2023-12-26 12:26:16 +0500

    update valentina

--- test.py
+++ test.py
@@ -1,27 +1,22 @@
-def lizard_existence_days_seconds(start, end):
-    days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-    year1, month1, day1, hour1, min1, sec1 = map(int, start.split())
-    year2, month2, day2, hour2, min2, sec2 = map(int, end.split())
+u = [16000]  # начальное значение u_1
+c = 190000 / 101  # значение константы c
 
-    # Коррекция индексов для массива дней в месяце
-    month1 -= 1
-    month2 -= 1
+# Проверка рекуррентного соотношения
+for n in range(1, 100):  # проверяем для первых 100 месяцев
+    # Вычисляем u_{n+1} с помощью вашего решения и рекуррентного соотношения
+    u_n_plus_1_from_solution = c * 1.01**(n+1) - 35000
+    u_n_plus_1_from_recurrence = 1.01 * u[-1] - 350
 
-    full_years = year2 - year1 - 1
+    # Сравниваем два значения u_{n+1}
+    if abs(u_n_plus_1_from_solution - u_n_plus_1_from_recurrence) > 1e-6:  # разница больше, чем допустимая погрешность
+        print(f"Решение неверно для n = {n}")
+        break
 
-    # Рассчитываем количество полных дней в первом и последнем году
-    days_first_year = sum(days_in_month[month1:]) - day1
-    days_last_year = sum(days_in_month[:month2]) + day2
+    # Добавляем u_{n+1} в список
+    u.append(u_n_plus_1_from_recurrence)
 
-    total_days = full_years * 365 + days_first_year + days_last_year
-
-    seconds_in_partial_day = (hour2 * 3600 + min2 * 60 + sec2) - (hour1 * 3600 + min1 * 60 + sec1)
-
-    return total_days, seconds_in_partial_day
-
-start_date = input()
-end_date = input()
-
-result = lizard_existence_days_seconds(start_date, end_date)
-print(result[0], result[1])
+    # Проверяем, выплачен ли долг
+    if u[-1] <= 0:
+        print(f"Долг будет выплачен через {n+1} месяцев")
+        break
