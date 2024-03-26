bf97a04bff2a286a783b0ced3cca1cf8bf09de94
Beloff Lab <belofflab@gmail.com>
2024-03-26 16:47:08 +0500

    num course update

--- numerologViktoria/handlers/user/numercourse.py
+++ numerologViktoria/handlers/user/numercourse.py
@@ -87,7 +87,7 @@ askeza_buttons = {
         "description": f"""
 Чтобы задать интересующий вопрос или записаться на обучение напишите мне в личные слово «Обучение» ⬇️
 
-@{V_WORKER_USERNAME} 
+@{WORKER_USERNAME} 
 """,
     "whatsapp_btn": "https://api.whatsapp.com/send/?phone=79292084866&text=Хочу+узнать+подробнее+об+обучении&type=phone_number&app_absent=0?"
     }
